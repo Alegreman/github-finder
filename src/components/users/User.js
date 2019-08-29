a84a42dafa3c3680edd5885b87b7dd3c5ad2c5ec
alegreman <alegreman09@gmail.com>
2019-08-29 13:48:13 -0600

    added hireable state

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,15 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-class User extends Component {
+export class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
   }
+
+  static propTypes = {
+    loading: PropTypes.bool.isRequired,
+    user: PropTypes.object.isRequired,
+    getUser: PropTypes.func.isRequired
+  };
+
   render() {
     const {
       name,
       avatar_url,
       location,
       bio,
+      hireable,
       blog,
       login,
       html_url,
@@ -21,7 +32,21 @@ class User extends Component {
 
     const { loading } = this.props;
 
-    return <div>{name}</div>;
+    if (loading) return <Spinner />;
+
+    return (
+      <Fragment>
+        <Link to='/' className='btn btn-light'>
+          Back to Search
+        </Link>
+        Hireable:{' '}
+        {hireable ? (
+          <i className='fas fa-check text-success' />
+        ) : (
+          <i className='fas fa-times-circle text-danger' />
+        )}
+      </Fragment>
+    );
   }
 }
 
