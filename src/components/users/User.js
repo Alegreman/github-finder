7cc6806bb9dabf3738df453e3e82be3378880e1e
alegreman <alegreman09@gmail.com>
2019-09-12 11:58:25 -0600

    added badges to profile

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -23,11 +23,12 @@ export class User extends Component {
       hireable,
       blog,
       login,
+      company,
       html_url,
       followers,
       following,
       public_repos,
-      public_gists_hireable
+      public_gists
     } = this.props.user;
 
     const { loading } = this.props;
@@ -45,6 +46,61 @@ export class User extends Component {
         ) : (
           <i className='fas fa-times-circle text-danger' />
         )}
+        <div className='card grid-2'>
+          <div className='all-center'>
+            <img
+              src={avatar_url}
+              className='round-img'
+              alt=''
+              style={{ width: '150px' }}
+            />
+            <h1>Location: {location}</h1>
+          </div>
+          <div>
+            {bio && (
+              <Fragment>
+                <h3>Bio</h3>
+                <p>{bio}</p>
+              </Fragment>
+            )}
+
+            <a href={html_url} className='btn btn-dark my-1'>
+              Visit Github Profile
+            </a>
+            <ul>
+              <li>
+                {login && (
+                  <Fragment>
+                    <strong>Username:</strong>
+                    {login}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {company && (
+                  <Fragment>
+                    <strong>Company:</strong>
+                    {company}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {blog && (
+                  <Fragment>
+                    <strong>Website:</strong>
+                    {blog}
+                  </Fragment>
+                )}
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className='card text-center'>
+          <div className='badge badge-primary'>Followers: {followers}</div>
+          <div className='badge badge-success'>Following: {following}</div>
+          <div className='badge badge-light'>Public Repos: {public_repos}</div>
+          <div className='badge badge-dark'>Public Gists: {public_gists}</div>
+        </div>
       </Fragment>
     );
   }
